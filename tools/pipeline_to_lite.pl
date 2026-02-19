1a398ef55d624c1d54bb12013501fd457797591b
Brett Whitty <brett@gnomatix.com>
2026-02-19 15:05:03 -0500

    Trim unnecessary top-level serial nesting in .lite files
    pipeline.layout XML has commandSetRoot > commandSet(serial) wrapping
    the actual pipeline structure. The .lite generator was emitting both
    levels, producing ( ( ... ) ) when ( ... ) is correct.
    
    Fixed pipeline_to_lite.pl to unwrap top-level instance/serial containers
    before generating .lite notation. Also trims redundant same-type nesting
    within the tree (serial wrapping single serial child).
    
    33 of 40 pipeline .lite files affected.

--- tools/pipeline_to_lite.pl
+++ tools/pipeline_to_lite.pl
@@ -2,15 +2,26 @@
 use strict;
 use warnings;
 use JSON;
-use File::Slurp;
+
 use File::Basename;
 
 my $input = shift or die "Usage: $0 <pipeline.json>\n";
-my $data = from_json(read_file($input));
+my $data = do { open my $fh, "<", $input or die "$!"; local $/; decode_json(<$fh>) };
 
 my $name = $data->{name} || basename($input, '.json');
 print "# $name\n";
-print node_to_lite($data->{structure}, 0), "\n";
+
+# Unwrap top-level container (type=instance or bare serial wrapper)
+my $structure = $data->{structure};
+while ($structure->{children} 
+       && @{$structure->{children}} == 1
+       && ref($structure->{children}[0]) eq 'HASH'
+       && $structure->{children}[0]{children}
+       && ($structure->{type} || '') ne 'parallel') {
+    $structure = $structure->{children}[0];
+}
+
+print node_to_lite($structure, 0), "\n";
 
 sub node_to_lite {
     my ($node, $depth) = @_;
@@ -22,6 +33,14 @@ sub node_to_lite {
     
     my $children = $node->{children} || [];
     return "" unless @$children;
+    
+    # Trim unnecessary nesting: serial with single serial child, or parallel with single parallel child
+    while (@$children == 1 
+           && ref($children->[0]) eq 'HASH' 
+           && ($children->[0]{type} || '') eq ($node->{type} || '')
+           && $children->[0]{children}) {
+        $children = $children->[0]{children};
+    }
     
     # Determine bracket type from node type
     my $open  = '(';
